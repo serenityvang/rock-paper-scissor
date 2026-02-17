e74fae723d59a94d77640ab33967533836fd55fc
Serenity Vang <serenvang23@gmail.com>
2026-02-16 21:24:35 -0600

    removed pseudocode comment

--- game.js
+++ game.js
@@ -1,67 +1,3 @@
-/*
-This game of rock paper or scissors will be against a computer. 
-
-COMPUTER:
-getComputerChoice will randomly return one of the following string values "rock", "paper", or "scissors".
-
-we need to create a variable to store the string rock paper or scissors
-
-To randomly return we can use Math.random();
-math.random() is from 0-1
-
-if computer is randomly >= 0.3 then computer choice is rock
-if computer is randomly >= 0.6 then computer choice is paper
-else computer choice is scissors 
-
-Player:
-
-User enters "rock", "paper", or "scissors"
-and returns user inputs
-use prompt
-
-Play 1 round:
-
-two parameters for the function playRound - humanChoice, computerChoice.
-
-humanChoice should be case-insensitive so that if players input rock, ROCK, RocK it wont matter and will work
-
-console.log a string value for the winner
-
-and increment the score for human or computer based on the round winner.
-
-logic: if human choice equals computer choice 
-announce draw
-
-if human chooses rock and computer chooses scissors
-human win
-increase human score
-
-if human chooses paper and computer choose rock
-announce human wins 
-increase human score
-
-if human chooses scissors and computer chosses paper
-announce human wins
-increase human score
-
-otherwise computer wins 
-announce human score
-increase computerscore 
-
-playGame():
-
-we want to call the playRound method 5 times for a game
-    each playRound we want to print out who wins and the score
-
-    we need to get the humanChoice and the computerChoice before calling playRound.
-
-    We need to see what result is during each round if it is human or computer. If human one then update the humanScore by one, else if computer won update the computerscore by one.
-
-
-if human score is greater than computer score than human wins
-if computer score is greater than human than computer wins
-
-*/
 
 function getComputerChoice() {
   let random = Math.random();
@@ -133,7 +69,7 @@ function playGame() {
       computerScore++;
     }
 
-    console.log(`Score: you ${humanScore} - Computer ${computerScore}`);
+    console.log(`Score: You ${humanScore} - Computer ${computerScore}`);
 
   }
 

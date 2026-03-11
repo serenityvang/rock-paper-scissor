72e1efa5d9760dca9fd459d614b8fbc42aa86992
Serenity Vang <serenvang23@gmail.com>
2026-03-11 12:18:27 -0500

    fix formatting and remove whitespace

--- game.js
+++ game.js
@@ -13,148 +13,142 @@ function getComputerChoice() {
   }
 }
 
-  // divs 
-  const div = document.querySelector(".rsp-buttons");
-  const results = document.querySelector(".scoreAndSpan");
+// divs
+const div = document.querySelector(".rsp-buttons");
+const results = document.querySelector(".scoreAndSpan");
 
-  // buttons
-  const rock = document.createElement("button");
-  const paper = document.createElement("button");
-  const scissors = document.createElement("button");
-  const reset = document.createElement("button");
+// buttons
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+const reset = document.createElement("button");
 
-  //score and results
-  const sumText = document.createElement("span");
-  const score = document.createElement("span");
-  
-  // text content
-  rock.textContent = "Rock";
-  paper.textContent = "Paper";
-  scissors.textContent = "Scissors";
-  reset.textContent = "Reset";
+//score and results
+const sumText = document.createElement("span");
+const score = document.createElement("span");
 
-  // styling reset button
-  reset.classList.add("resetbtn");
-  
+// text content
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+reset.textContent = "Reset";
 
+// styling reset button
+reset.classList.add("resetbtn");
 
-  div.appendChild(rock);
-  div.appendChild(paper);
-  div.appendChild(scissors);
-  div.appendChild(reset);
-  results.appendChild(sumText);
-  results.appendChild(score);
+// appending child to parent
+div.appendChild(rock);
+div.appendChild(paper);
+div.appendChild(scissors);
+div.appendChild(reset);
+results.appendChild(sumText);
+results.appendChild(score);
 
-  // Game State
-  let humanScore = 0;
-  let computerScore = 0;
+// Game State
+let humanScore = 0;
+let computerScore = 0;
 
+// expects a function, not the result of the function call
+// does score and calls the playRound function
+// checks if there is a winning score
+rock.addEventListener("click", () => {
+  console.log("rock button click");
+  const result = playRound("rock");
 
-  // expects a function, not the result of the function call
-  // does score and calls the playRound function
-  // checks if there is a winning score
-  rock.addEventListener("click", () => {
+  if (result === "human") {
+    humanScore++;
+  } else if (result === "computer") {
+    computerScore++;
+  }
 
-    console.log("rock button click");
-    const result = playRound("rock");
+  score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
 
-    if(result === "human"){
-      humanScore++;
-    } else if (result === "computer"){
-      computerScore++;
-    }
+  if (humanScore === 3) {
+    sumText.textContent = "You won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 
-    score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
+  if (computerScore === 3) {
+    sumText.textContent = "Computer won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
+});
 
-    if(humanScore === 3){
-      sumText.textContent = "You won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    } 
-    
-    if(computerScore === 3){
-      sumText.textContent = "Computer won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    }
-  });
+paper.addEventListener("click", () => {
+  console.log("paper button click");
+  const result = playRound("paper");
 
+  if (result === "human") {
+    humanScore++;
+  } else if (result === "computer") {
+    computerScore++;
+  }
 
-  paper.addEventListener("click", () => {
+  score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
 
-    console.log("paper button click");
-    const result = playRound("paper");
+  if (humanScore === 3) {
+    sumText.textContent = "You won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 
-    if(result === "human"){
-      humanScore++;
-    } else if (result === "computer"){
-      computerScore++;
-    }
+  if (computerScore === 3) {
+    sumText.textContent = "Computer won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
+});
 
-    score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
+scissors.addEventListener("click", () => {
+  console.log("scissors button click");
+  const result = playRound("scissors");
 
-    if(humanScore === 3){
-      sumText.textContent = "You won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    } 
-    
-    if(computerScore === 3){
-      sumText.textContent = "Computer won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    }
-  });
+  if (result === "human") {
+    humanScore++;
+  } else if (result === "computer") {
+    computerScore++;
+  }
 
+  score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
 
-  scissors.addEventListener("click", () => {
+  if (humanScore === 3) {
+    sumText.textContent = "You won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 
-    console.log("scissors button click");
-    const result = playRound("scissors");
+  if (computerScore === 3) {
+    sumText.textContent = "Computer won the game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
+});
 
-     if(result === "human"){
-      humanScore++;
-    } else if (result === "computer"){
-      computerScore++;
-    }
+reset.addEventListener("click", () => {
+  resetGame();
+});
 
-    score.textContent = `Human Score: ${humanScore} - Computer Score: ${computerScore}`;
-
-    if(humanScore === 3){
-      sumText.textContent = "You won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-      
-    } 
-    
-    if(computerScore === 3){
-      sumText.textContent = "Computer won the game!";
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    }
-  });
-
-  reset.addEventListener("click", () => {
-    resetGame();
-  });
-
-  // checks human choice against computers choice
-function playRound(humanChoice){
+// checks human choice against computers choice
+function playRound(humanChoice) {
   let computerChoice = getComputerChoice();
 
   if (humanChoice == computerChoice) {
-    console.log(`Draw! Human Choice: ${humanChoice} is tied with Computers Choice: ${computerChoice}`);
+    console.log(
+      `Draw! Human Choice: ${humanChoice} is tied with Computers Choice: ${computerChoice}`,
+    );
     sumText.textContent = `Draw! Human Choice: ${humanChoice}, is tied with Computers Choice: ${computerChoice}`;
     return "draw";
   } else if (humanChoice == "rock" && computerChoice == "scissors") {
     console.log("Human Wins! rock beats scissors");
-     sumText.textContent = "Human Wins! rock beats scissors";
+    sumText.textContent = "Human Wins! rock beats scissors";
     return "human";
   } else if (humanChoice == "paper" && computerChoice == "rock") {
     console.log("Human Wins! paper beats rock");
@@ -169,7 +163,6 @@ function playRound(humanChoice){
     sumText.textContent = `Computer Wins! ${computerChoice} beats ${humanChoice}`;
     return "computer";
   }
-
 }
 
 function resetGame() {
